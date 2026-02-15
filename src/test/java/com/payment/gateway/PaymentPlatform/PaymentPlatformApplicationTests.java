585a7ea0a66699a9c240edae02323be0f6241b32
Atharva1512 <borawakeatharva2002@gmail.com>
2026-02-15 13:08:44 +0530

    Initial commit

---
+++ src/test/java/com/payment/gateway/PaymentPlatform/PaymentPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.gateway.PaymentPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
